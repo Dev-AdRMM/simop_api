a298e97eadda94c6de0ac3ac1c20fcc9d4c770b9
Dev AdRMM <devadrmm@gmail.com>
2025-08-14 16:44:04 +0200

    api routes register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MkeshCallbackController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/mkesh/callback', [MkeshCallbackController::class, 'handle']);
