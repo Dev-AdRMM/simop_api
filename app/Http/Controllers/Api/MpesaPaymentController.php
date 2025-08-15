6fe1d892c0de1cf07d7dde4f4e2e7c4b258b9e4a
Dev AdRMM <devadrmm@gmail.com>
2025-08-15 15:37:52 +0200

    chore():MpesaPaymentController  and MkeshCallbackController

---
+++ app/Http/Controllers/Api/MpesaPaymentController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Services\PaymentGateways\MpesaPaymentService;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MpesaPaymentController extends Controller
+{
+    private $mpesaService;
+
+    public function __construct(MpesaPaymentService $mpesaService)
+    {
+        $this->mpesaService = $mpesaService;
+    }
+
+    public function processPayment(Request $request)
+    {
+        try {
+            $request->validate([
+                'phone_number' => 'required|regex:/^258\d{9}$/',
+                'amount' => 'required|numeric|min:1',
+            ]);
+
+            $invoice_number = 3333;
+            $phone_number = $request->input('phone_number');
+            $amount = $request->input('amount');
+
+            $result = $this->mpesaService->purchase($invoice_number, $phone_number, $amount);
+
+            if ($result['status'] === 'success') {
+                return response()->json([
+                    'status' => 'success',
+                    'message' => $result['message'],
+                    'transaction_id' => $result['transaction_id'],
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'failed',
+                    'message' => $result['message'],
+                ], 400);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MkeshCallbackController;
+use App\Http\Controllers\Api\MkeshCallbackController;
+use App\Http\Controllers\Api\MpesaPaymentController;
 
+
+#Api Payment
 Route::post('/mkesh/callback', [MkeshCallbackController::class, 'handle']);
+
+#mpesa
+Route::post('/process-payment', [MpesaPaymentController::class, 'processPayment'])->name('process.payment');
