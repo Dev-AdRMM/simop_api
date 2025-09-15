290a3d8364c99da81165144f935c45b0cfb9a779
Dev AdRMM <devadrmm@gmail.com>
2025-09-15 15:08:11 +0200

    fix className Traits

--- app/Traits/ApiLogsTransactions.php
+++ app/Traits/ApiLogsTransactions.php
@@ -4,7 +4,7 @@ namespace App\Traits;
 
 use App\Models\SimopApiLogsTransactions;
 
-trait LogsApi
+trait ApiLogsTransactions
 {
     public function logApi($endpoint, $method, $headers, $payload, $response = null)
     {
